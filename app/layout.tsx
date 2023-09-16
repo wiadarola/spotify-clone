f6652994c554249d03f6b84a7ad729b16750e7e4
William Iadarola <wiadarola0@gmail.com>
2023-09-16 11:14:06 -0700

    Logout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import SubabaseProvider from '@/providers/SupabaseProvider'
 import UserProvider from '@/providers/UserProvider'
 import Sidebar from '@/components/Sidebar'
 import ModalProvider from '@/providers/ModalProvider'
+import ToasterProvider from '@/providers/ToasterProvider'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
         <SubabaseProvider>
           <UserProvider>
             <ModalProvider />
--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,7 @@ import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { FaUserAlt } from "react-icons/fa";
+import { toast } from "react-hot-toast";
 
 import Button from "./Button";
 import useAuthModal from "@/hooks/useAuthModel";
@@ -30,7 +31,9 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
         router.refresh();
 
         if (error) {
-            console.log(error);
+            toast.error(error.message);
+        } else {
+            toast.success('Logged out successfully!');
         }
     };
 

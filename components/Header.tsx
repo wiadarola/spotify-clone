9da1d68da8a205db542c6b951d121b55ada64ed2
William Iadarola <wiadarola0@gmail.com>
2023-09-16 18:37:31 -0700

    Reset playing songs on logout

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,6 +12,7 @@ import { toast } from "react-hot-toast";
 import Button from "./Button";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
+import usePlayer from "@/hooks/usePlayer";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -25,9 +26,11 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
     const supabaseClient = useSupabaseClient();
     const { user } = useUser();
 
+    const player = usePlayer();
+
     const handleLogout = async () => {
         const { error } = await supabaseClient.auth.signOut();
-        // TODO: Reset any playing songs
+        player.reset();
         router.refresh();
 
         if (error) {
